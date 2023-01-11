0fb39db313332e84758e0a4598e2fe62e538bc62
Crash-- <quentin.valmori@gmail.com>
2023-01-11 10:22:02 +0100

    add import * from models ex

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
+// => 980b
 //import { models } from './models/exportall'
 //console.log(models.baz.foo)
 
+// => 3.X Kb
+//import {baz} from './models/index'
+//console.log(baz.foo)
 
-import {baz} from './models/index'
-console.log(baz.foo)
+// => 970b
+import * as a from './models'
+console.log(a.foo)
